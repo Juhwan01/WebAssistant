bbd3751dd900e524ff8d28644b1612b92c748433
Juhwan01 <wnghks5432@naver.com>
2024-06-26 02:14:12 +0900

    06.26
    웹 챗봇 초안

---
+++ LangChain/WebBot.py
@@ -0,0 +1,76 @@
+# API 키를 환경변수로 관리하기 위한 설정 파일
+from dotenv import load_dotenv
+
+# API 키 정보 로드
+load_dotenv()
+
+# LangSmith 추적을 설정합니다. https://smith.langchain.com
+from langchain_teddynote import logging
+
+# 프로젝트 이름을 입력합니다.
+logging.langsmith("hackathon")
+
+import bs4
+from langchain import hub
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+# 뉴스기사의 내용을 로드하고, 청크로 나누고, 인덱싱합니다.
+loader = WebBaseLoader(
+    web_paths=("https://www.bbc.com/news/business-68092814",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            "main",
+            attrs={"id": ["main-content"]},
+        )
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+splits = text_splitter.split_documents(docs)
+len(splits)
+
+# 벡터스토어를 생성합니다.
+vectorstore = FAISS.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+# 뉴스에 포함되어 있는 정보를 검색하고 생성합니다.
+retriever = vectorstore.as_retriever()
+
+
+from langchain_core.prompts import PromptTemplate
+
+prompt = PromptTemplate.from_template(
+    """당신은 질문-답변(Question-Answering)을 수행하는 친절한 AI 어시스턴트입니다. 당신의 임무는 주어진 문맥(context) 에서 주어진 질문(question) 에 답하는 것입니다.
+검색된 다음 문맥(context) 을 사용하여 질문(question) 에 답하세요. 만약, 주어진 문맥(context) 에서 답을 찾을 수 없다면, 답을 모른다면 `주어진 정보에서 질문에 대한 정보를 찾을 수 없습니다` 라고 답하세요.
+한글로 답변해 주세요. 단, 기술적인 용어나 이름은 번역하지 않고 그대로 사용해 주세요.
+
+#Question: 
+{question} 
+
+#Context: 
+{context} 
+
+#Answer:"""
+)
+
+llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
+
+
+# 체인을 생성합니다.
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+from langchain_teddynote.messages import stream_response
+
+answer = rag_chain.stream("내용 요약해줘?")
+stream_response(answer)
